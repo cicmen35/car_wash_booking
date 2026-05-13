46bc3ce7ca791fb91598899da81406f1229edb52
cicmen35 <138312285+cicmen35@users.noreply.github.com>
2026-05-13 13:12:51 +0200

    chore: initialize spring boot backend with gradle, h2, jpaand security config

---
+++ backend/src/main/java/com/autoglow/carwash/CarwashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.autoglow.carwash;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarwashBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarwashBackendApplication.class, args);
+	}
+
+}
